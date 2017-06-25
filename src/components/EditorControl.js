a92cf72d112b0d649c67deae6783f3a9bd228407
Francesco Agosti <francesco.agosti93@gmail.com>
2017-06-24 17:44:40 -0700

    Sort selects

--- src/components/EditorControl.js
+++ src/components/EditorControl.js
@@ -40,8 +40,7 @@ export const languages = [
   'matlab',
   'php',
   'rust',
-
-];
+].sort();
 
 export const themes = [
   'monokai',
@@ -58,7 +57,7 @@ export const themes = [
   'eclipse',
   'dawn',
   'textmate',
-];
+].sort();
 
 export const fontSizes = [6,7,8,9,10,11,12,13,14,15,16,18,20,22,24,26,28,30,32,34,36];
 
