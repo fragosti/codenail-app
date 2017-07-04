2d1ab35fe1030e3c66734de78fadf37af89f8afe
Francesco Agosti <francesco.agosti93@gmail.com>
2017-07-03 23:13:03 -0700

    Add order sharing on facebook and twitter

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,15 +3,27 @@ import styled from 'styled-components';
 
 const Container = styled.span`
   position: relative;
-  top: ${props => props.top}px;
+  top: ${props => props.top || 0}px;
 `
 
-const icon = (name, size) => {
+const icon = (name, size = 24, fill = "none") => {
   return {
     edit: (
       <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24">
-        <polygon points="14 2 18 6 7 17 3 17 3 13 14 2" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
+        <polygon points="14 2 18 6 7 17 3 17 3 13 14 2" fill={fill} stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
         <line x1="3" y1="22" x2="21" y2="22" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
+      </svg>
+    ),
+    facebook: (
+      <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" 
+        fill={fill} stroke="#000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+      </svg>
+    ),
+    twitter: (
+      <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24">
+        <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" 
+          fill={fill} stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
       </svg>
     )
   }[name]
--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { ShareButtons } from 'react-share';
 
 import OrderPreview from './OrderPreview';
 import Spinner from './Spinner';
+import Icon from './Icon';
 import { getOrder } from '../lib/api';
 import { Container, Description } from './Page';
+
+const { FacebookShareButton, TwitterShareButton } = ShareButtons
 
 /* eslint-disable jsx-a11y/accessible-emoji */
 const Emoji = styled.span`
@@ -12,6 +16,22 @@ const Emoji = styled.span`
   margin: 10px;
   position: relative;
   top: 3px;
+`
+
+const SocialShares = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+
+const ShareIconWrap = styled.div`
+  margin-top: 40px;
+  margin-right: 25px;
+  margin-left: 25px;
+  cursor: pointer;
+  &:hover path {
+    fill: ${props => props.hoverColor};
+  }
 `
 
 class ThankYou extends Component {
@@ -41,6 +61,14 @@ class ThankYou extends Component {
     const { id } = this.props.match.params
     const { isLoading } = this.props
     const { firstName, description, email } = this.state
+    const social = {
+      title: "Codenail",
+      description: "Just ordered this awesome poster on Codenail!",
+      url: "https://codenail.com/",
+      picture: `https://s3-us-west-2.amazonaws.com/codenail-order-previews/${id}.png`,
+      hashtags: ['art', 'code', 'poster']
+    }
+
     return (
       <Container> 
         <Description>
@@ -48,7 +76,30 @@ class ThankYou extends Component {
           {isLoading ? <Spinner/> : (
             <p> Hi {firstName}, your {description} will be arriving shortly. A confirmation e-mail has been sent to <strong>{email}</strong>. </p>
           )}
+          <SocialShares>
+            <FacebookShareButton 
+              title={social.title}
+              description={social.description}
+              url={social.url}
+              picture={social.picture}
+            >
+              <ShareIconWrap hoverColor='#3B5998'>
+                <Icon name='facebook' size={40}/>
+              </ShareIconWrap>
+            </FacebookShareButton>
+            <TwitterShareButton 
+              title={social.title}
+              url={social.url}
+              via='codenail'
+              hashtags={social.hashtags}
+            >
+              <ShareIconWrap hoverColor='#00aced'>
+                <Icon name='twitter' size={40}/>
+              </ShareIconWrap>
+            </TwitterShareButton>
+          </SocialShares>
           <Container>
+            <p>Don't forget to share!</p>
             <OrderPreview id={id}/>
           </Container>
         </Description>
