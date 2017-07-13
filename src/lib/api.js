08a1e18ef2b22420f1a152d95098b63f2e332523
Francesco Agosti <francesco.agosti93@gmail.com>
2017-07-12 21:01:22 -0700

    Read API endpoint fron dotenv

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,18 +1,6 @@
-const getEndpoint = (env) => {
-  switch(env) {
-    case 'staging':
-      return ''
-    case 'production':
-      return 'https://t46e391x1a.execute-api.us-west-2.amazonaws.com/production'
-    default:
-    case 'development':
-      return 'http://localhost:8000/dev'
-  }
-}
-
 export const TEST_STRIPE_KEY = 'pk_test_lQYC49aP6fT12LuZv8ejgghF'
 export const STRIPE_KEY = process.env.REACT_APP_STRIPE_KEY
-export const API_ENDPOINT = getEndpoint(process.env.NODE_ENV)
+export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
 
 export const createOrder = (order) => {
   return fetch(`${API_ENDPOINT}/order`, {
