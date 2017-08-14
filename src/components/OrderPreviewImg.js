08fb96f8026f3e997e0067ffbd65178a4889e79b
Francesco Agosti <francesco.agosti93@gmail.com>
2017-08-13 23:55:16 -0700

    Add border around preview image

--- src/components/OrderPreviewImg.js
+++ src/components/OrderPreviewImg.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Image from './Image';
 
+import { colors } from '../style/utils';
+
+const PrevImage = Image.extend`
+  border: 2px solid ${colors.gray};
+  border-radius: 3px;
+`
+
 const OrderPreviewImg = ({id, width}) => (
-  <Image alt='Order preview' width={width} src={`https://s3-us-west-2.amazonaws.com/codenail-order-previews/${id}.png`}/>
+  <PrevImage alt='Order preview' width={width} src={`https://s3-us-west-2.amazonaws.com/codenail-order-previews/${id}.png`}/>
 )
 
 export default OrderPreviewImg
