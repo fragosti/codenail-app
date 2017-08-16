faf0c77677c74cba1ecebf3ee27cbc97982c888e
Francesco Agosti <francesco.agosti93@gmail.com>
2017-08-15 23:20:30 -0700

    Refactor Order component

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,124 +1,28 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-import Flex from './Flex';
-import withLoading from '../HOCs/withLoading';
-import CheckoutButton from './CheckoutButton';
-import { Callout } from './Button';
-import { createOrder } from '../lib/api';
-import Spinner from './Spinner';
-import { isPhone, colors } from '../style/utils';
-import { Message, SpacedCTA } from './Modal';
+import GenericOrder from './GenericOrder';
 
-const Table = styled.table`
-  border: 2px dashed ${colors.gray};
-  margin-bottom: 25px;
-  border-radius: 4px;
-  width: 340px;
-  margin-left: auto;
-  margin-right: auto;
-  tr {
-    text-align: center;
+
+const Download = ({ price, description, isTest, options, history, search, openModal}) => {
+  const summaryMap = {
+    'Order Type': 'high resolution poster PNG file',
+    'Order Price': `$${price}`,
+    'Delivery Method': 'download link',
+    'Dimensions': `${options.size} inches`,
   }
-  td {
-    padding: 10px;
-  }
-`
-
-const LoadingMessage = styled.div`
-  margin-top: 20px;
-  font-weight: 600;
-`
-
-const SummaryTable = ({ summaryMap }) => (
-  <Table>
-    <tbody>
-      {Object.keys(summaryMap).map((label) => (
-        <tr key={label}>
-          <td><strong>{label}: </strong></td> 
-          <td>{summaryMap[label]}</td>
-        </tr>
-      ))}
-    </tbody>
-  </Table>
-)
-
-class Download extends Component {
-  state = {
-    errorMessage: null,
-  }
-
-  render() {
-    const { price, description, isTest, options, history, search, openModal, isLoading, loadingMessage, startLoading, stopLoading } = this.props
-    const summaryMap = {
-      'Order Type': 'high resolution poster PNG file',
-      'Order Price': `$${price}`,
-      'Delivery Method': 'download link',
-      'Dimensions': `${options.size} inches`,
-    }
-    const { errorMessage } = this.state
-    return (isLoading ? (
-      <Flex direction='column' alignItems='center'>
-        <Spinner scale={6} rgbaColor={colors.gray}/>
-        <LoadingMessage>{loadingMessage}</LoadingMessage>
-      </Flex>
-    ) : (
-      <Flex direction='column' alignItems='center'>
-        {errorMessage && <i>{errorMessage}</i>}
-        <Message>
-          <SummaryTable summaryMap={summaryMap}/>
-          <p>
-            Once you order you'll receive a confirmation e-mail with an order receipt and download link.
-          </p>
-          <p>
-            More questions? Visit our <Link to='faq'><Callout>FAQ</Callout></Link> page. 
-          </p>
-        </Message>
-        <Flex justifyContent='center'>
-          <SpacedCTA onClick={() => openModal('preview')}> Preview </SpacedCTA>
-          <CheckoutButton
-            onToken={(token, addresses) => {
-              startLoading('Processing your order...')
-              createOrder({
-                token,
-                addresses,
-                price: price*100,
-                description,
-                isTest,
-                isPhone: isPhone(),
-                options,
-                justDownload: true,
-              })
-              .then(res => res.json())
-              .then(({ id }) => {
-                openModal('thankyou', id)
-              })
-              .catch((error) => {
-                stopLoading()
-                console.log(error)
-                this.setState({ errorMessage: 'Sorry, something went wrong. Please try again later.'})
-              })
-            }}
-            price={price*100}
-            description={description}
-            isTest={isTest}
-            opened={() => {
-              if (search) {
-                history.push(`/create${search}&overlay=checkout`)
-              } else {
-                history.push(`/create?overlay=checkout`)
-              }
-            }}
-            closed={() => history.goBack()}
-          >
-            <SpacedCTA color={colors.green}>Purchase</SpacedCTA>
-          </CheckoutButton>
-        </Flex>
-      </Flex>
-    ))
-  }
+  return (
+    <GenericOrder
+      summaryMap={summaryMap}
+      justDownload={true}
+      price={price}
+      description={description}
+      isTest={isTest}
+      options={options}
+      history={history}
+      search={search}
+      openModal={openModal}
+    />
+  )
 }
 
-
-export default withLoading(Download)
+export default Download
---
+++ src/components/GenericOrder.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import Flex from './Flex';
+import withLoading from '../HOCs/withLoading';
+import CheckoutButton from './CheckoutButton';
+import { Callout } from './Button';
+import { createOrder } from '../lib/api';
+import Spinner from './Spinner';
+import { isPhone, colors } from '../style/utils';
+import { Message, SpacedCTA } from './Modal';
+
+const Table = styled.table`
+  border: 2px dashed ${colors.gray};
+  margin-bottom: 25px;
+  border-radius: 4px;
+  width: 325px;
+  margin-left: auto;
+  margin-right: auto;
+  tr {
+    text-align: center;
+  }
+  td {
+    padding: 10px;
+  }
+`
+
+const LoadingMessage = styled.div`
+  margin-top: 20px;
+  font-weight: 600;
+`
+
+const SummaryTable = ({ summaryMap }) => (
+  <Table>
+    <tbody>
+      {Object.keys(summaryMap).map((label) => (
+        <tr key={label}>
+          <td><strong>{label}: </strong></td> 
+          <td>{summaryMap[label]}</td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+)
+
+class GenericOrder extends Component {
+  state = {
+    errorMessage: null,
+  }
+
+  render() {
+    const { summaryMap, justDownload, price, description, isTest, options, history, search, openModal, isLoading, loadingMessage, startLoading, stopLoading } = this.props
+    const { errorMessage } = this.state
+    return (isLoading ? (
+      <Flex direction='column' alignItems='center'>
+        <Spinner scale={6} rgbaColor={colors.gray}/>
+        <LoadingMessage>{loadingMessage}</LoadingMessage>
+      </Flex>
+    ) : (
+      <Flex direction='column' alignItems='center'>
+        {errorMessage && <i>{errorMessage}</i>}
+        <Message>
+          <SummaryTable summaryMap={summaryMap}/>
+          <p>
+            Once you order you'll receive a confirmation e-mail with an order receipt and more information.
+          </p>
+          <p>
+            More questions? Visit our <Link to='faq'><Callout>FAQ</Callout></Link> page. 
+          </p>
+        </Message>
+        <Flex justifyContent='center'>
+          <SpacedCTA onClick={() => openModal('preview')}> Preview </SpacedCTA>
+          <CheckoutButton
+            onToken={(token, addresses) => {
+              startLoading('Processing your order...')
+              createOrder({
+                token,
+                addresses,
+                price: price*100,
+                description,
+                isTest,
+                isPhone: isPhone(),
+                options,
+                justDownload,
+              })
+              .then(res => res.json())
+              .then(({ id }) => {
+                openModal('thankyou', id)
+              })
+              .catch((error) => {
+                stopLoading()
+                console.log(error)
+                this.setState({ errorMessage: 'Sorry, something went wrong. Please try again later.'})
+              })
+            }}
+            price={price*100}
+            description={description}
+            isTest={isTest}
+            opened={() => {
+              if (search) {
+                history.push(`/create${search}&overlay=checkout`)
+              } else {
+                history.push(`/create?overlay=checkout`)
+              }
+            }}
+            closed={() => history.goBack()}
+          >
+            <SpacedCTA color={colors.green}>Purchase</SpacedCTA>
+          </CheckoutButton>
+        </Flex>
+      </Flex>
+    ))
+  }
+}
+
+
+export default withLoading(GenericOrder)
